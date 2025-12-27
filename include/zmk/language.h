d6d4b69a64e3415d1b74c1b6aa1f9e408c58d782
mikhail.karbat <mikhail.karbat@proton.me>
2025-12-27 14:49:32 +0300

    глобальная переменная -- счётчик нажатых модификаторов

--- include/zmk/language.h
+++ include/zmk/language.h
@@ -9,7 +9,7 @@
 #include <stdint.h>
 
 // Задержка на переключение языка системы в мс
-#define KP_ON_LANG_DELAY_MS 80
+#define KP_ON_LANG_DELAY_MS 50
 
 // текущий язык клавиатуры -- 0 английский, 1 -- русский
 uint8_t get_kb_language();
@@ -18,3 +18,9 @@ void set_kb_language(uint8_t lang);
 // текущий язык компьютера -- 0 английский, 1 -- русский
 uint8_t get_os_language();
 void set_os_language(uint8_t lang);
+
+// счётчик нажатых на текущий момент модификаторов CTRL, ALT, CMD
+uint8_t get_modifiers_counter();
+void increment_modifiers_counter();
+void decrement_modifiers_counter();
+void reset_modifiers_counter();
--- src/language.c
+++ src/language.c
@@ -20,3 +20,26 @@ uint8_t get_os_language() {
 void set_os_language(uint8_t lang) {
         os_language_state = lang;
 }
+
+
+// счётчик нажатых на текущий момент модификаторов CTRL, ALT, CMD
+// используется чтобы модификаторы работали только на анлийской раскладке
+uint8_t modifiers_counter = 0;
+// получить значение счётчика нажатых модификаторов
+uint8_t get_modifiers_counter() { 
+    return modifiers_counter; 
+}
+// добавить 1 к счётчику нажатых модификаторов
+void increment_modifiers_counter() {
+    modifiers_counter++;
+}
+// уменьшить счётчик нажатых модификаторов на 1
+void decrement_modifiers_counter() {
+    if (modifiers_counter > 0) {
+        modifiers_counter--;
+    }
+}
+// обнулить счётчик нажатых модификаторов
+void reset_modifiers_counter() {
+    modifiers_counter = 0;
+}
